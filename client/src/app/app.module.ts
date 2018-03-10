7b6c32c13147deeab7812d115ef966649d42bdc7
Andrzej Heczko <andrzej.heczko@gmail.com>
2018-03-10 22:02:49 +0100

    wip search bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {SearchBarComponent} from "../pages/home/search-bar.component";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
@@ -27,4 +30,5 @@ import { HomePage } from '../pages/home/home';
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  public onSearchChange(str: string) {
 
   }
 
